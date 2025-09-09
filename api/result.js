d2d20b1ae6b9ab9c6771eb2f04dbf1be8a722e5f
bipinsingh1299uk-del <bipinsingh1299uk@gmail.com>
2025-09-09 23:37:01 +0530

    Update result.js

--- api/result.js
+++ api/result.js
@@ -2,11 +2,9 @@ export default async function handler(req, res) {
   const url = "https://91appt.com/api/game/xy28/xy28History";
 
   try {
-    // Node.js 18+ में fetch पहले से built-in है
     const response = await fetch(url);
     const data = await response.json();
 
-    // CORS allow
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
