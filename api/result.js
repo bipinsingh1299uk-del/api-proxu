cc15c68ce7eea2c02f8a2844e70f32fcc2eea605
bipinsingh1299uk-del <bipinsingh1299uk@gmail.com>
2025-09-09 23:15:44 +0530

    Update result.js

--- api/result.js
+++ api/result.js
@@ -1,10 +1,8 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
   const url = "https://91appt.com/api/game/xy28/xy28History";
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url); // node-fetch की जरूरत नहीं
     const data = await response.json();
 
     // CORS allow
@@ -12,6 +10,7 @@ export default async function handler(req, res) {
     res.status(200).json(data);
 
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Unable to fetch data" });
   }
 }
